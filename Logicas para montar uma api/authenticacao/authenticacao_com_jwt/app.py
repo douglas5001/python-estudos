ddb58b715e4eb27d376af5835fc57f9bbc76e93b
douglas5001 <douglasportella66@gmail.com>
2024-05-16 21:06:36 -0300

    criando crud usuario para authenticacao

--- Logicas para montar uma api/authenticacao/authenticacao_com_jwt/app.py
+++ Logicas para montar uma api/authenticacao/authenticacao_com_jwt/app.py
@@ -44,8 +44,8 @@ class Usuario():
         return self.__senha
     
     @senha.setter
-    def senha(self):
-        return self.__senha
+    def senha(self, senha):
+        self.senha = senha
     
 class UsuarioModel(db.Model):
     __tablename__ = 'usuario'
@@ -60,25 +60,104 @@ class UsuarioSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = UsuarioModel
         load_instance = True
-        fields = ('id','nome', 'email', 'senha')
+        fields = ("id","nome", "email", "senha")
     nome = fields.String(required=True)
     email = fields.String(required=True)
     senha = fields.String(required=True)
 
 def listarusuario():
-    usuario_db = UsuarioModel.Query.filter.All()
+    usuario_db = UsuarioModel.query.all()
     return usuario_db
 
-class usuariosList(Resource):
-    def get():
-        Usuario = listarusuario()
+def listar_usuario_id(id):
+    usuario_db = UsuarioModel.query.filter_by(id=id).first() #filt(id=id).first()
+    return usuario_db
+
+def cadastrar_usuario(usuario):
+    usuario_bd = UsuarioModel(nome=usuario.nome, email=usuario.email, senha=usuario.senha)
+    db.session.add(usuario_bd)
+    db.session.commit()
+    return usuario_bd
+
+def atualizar_tabela(usuario_anterior, usuario_novo):
+    usuario_anterior.nome = usuario_novo.nome
+    usuario_anterior.email = usuario_novo.email
+    usuario_anterior.senha = usuario_novo.senha
+    db.session.commit()
+
+def deletar_usuario(usuario):
+    db.session.delete(usuario)
+    db.session.commit()
+    return usuario
+
+
+class UsuariosList(Resource):
+    def get(self):
         us = UsuarioSchema(many=True)
-        produtos_serializados = us.dump(Usuario)  
-        return produtos_serializados 
+        resultado = listarusuario()
+        return make_response(us.jsonify(resultado), 200)
+    
+    def post(self):
+        us = UsuarioSchema()
+        validate  = us.validate(request.json)
+        if validate:
+            return make_response(jsonify(validate), 400)
+        else:
+            nome = request.json['nome']
+            email = request.json['email']
+            senha = request.json['senha']
+            novo_usuario = Usuario(nome=nome, email=email, senha=senha)
+            resultado = cadastrar_usuario(novo_usuario)
+            x = us.jsonify(resultado)
+            return make_response(x, 201)
+        
+class UsuarioIdList(Resource):
+
+    def get(self, id):
+        usuario = listar_usuario_id(id)
+        if usuario is None:
+            return make_response(jsonify('usuario nao encontrado'), 404)
+        else:
+            us = UsuarioSchema()
+
+            return make_response(us.jsonify(usuario), 200)
+
+    def put(self, id):
+        us = listar_usuario_id(id)
+        if us is None:
+            return make_response(jsonify('usuario Nao encontrado'), 404)
+        else:
+            us = UsuarioSchema()
+            validate = us.validate(request.json)
+            if validate:
+                return make_response(jsonify(validate), 400)
+            else:
+                nome = request.json['nome']
+                email = request.json['email']
+                senha = request.json['senha']
+                novo_usuario = Usuario(nome=nome, email=email, senha=senha)
+                atualizar_tabela(us, novo_usuario)
+                usuario_atualizado = listar_usuario_id(id)
+                return make_response(us.jsonify(usuario_atualizado), 201)
+    
+    def delete(self, id):
+        us = listar_usuario_id(id)
+        if us is None:
+            return make_response(jsonify('usuario nao encontrado'),404)
+        else:
+            deletar_usuario(us)
+            return make_response('usuario deletado')
+
+
+
+        
 
 
 
 
+
+api.add_resource(UsuariosList, '/usuarios')
+api.add_resource(UsuarioIdList, '/usuarios/<int:id>')
 
 
 
